665e8002d315c01145df9dfd7f642dc2800764d0
mohitbhagwani94 <mohitbhagwani94@gmail.com>
2017-01-16 14:22:27 +0530

    ============automate commiting 4============

--- my-script.sh
+++ my-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Start of Pull command"
+echo "============Start of Pull command=============="
 git pull
-echo "End of Pull command"
-echo "Start of Commit command"
+echo "============End of Pull command================"
+echo "============Start of Commit command============"
 git add .
-git commit -m "automate commiting 4"
-echo "End of Commit command"
-echo "Start of Push command"
+git commit -m "============automate commiting 4============"
+echo "============End of Commit command============"
+echo "============Start of Push command============"
 git push
-echo "End of Push command"
+echo "============End of Push command============"
