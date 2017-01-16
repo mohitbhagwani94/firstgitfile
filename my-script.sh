1bc3e15d76a65d5e1bb5b2a61d581a4ba542458a
mohitbhagwani94 <mohitbhagwani94@gmail.com>
2017-01-16 23:39:47 +0530

    adding git ignore

--- my-script.sh
+++ my-script.sh
@@ -4,6 +4,7 @@ git pull
 echo "============End of Pull command================"
 echo "============Start of Commit command============"
 git add .
+git reset my-script.sh
 git commit -m "============automate commiting 4============"
 echo "============End of Commit command============"
 echo "============Start of Push command============"
